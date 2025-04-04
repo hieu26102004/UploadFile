9d3718c18fc93aa1114cccd7666ba0b8090882c5
hieu26102004 <136700894+hieu26102004@users.noreply.github.com>
2025-04-04 07:55:18 +0700

    Initial commit

---
+++ app/src/main/java/com/example/uploadfile/ImageUpload.java
@@ -0,0 +1,31 @@
+package com.example.uploadfile;
+
+public class ImageUpload {
+    private int id;
+    private String username;
+    private String avatar;
+
+    public String getAvatar() {
+        return avatar;
+    }
+
+    public void setAvatar(String avatar) {
+        this.avatar = avatar;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
